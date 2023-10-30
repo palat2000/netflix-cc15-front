f1483a3e79ae37cfd4c5c6ee354583eefceabc70
Koblamer <RapeepornT.mar@gmail.com>
2023-10-30 15:35:24 +0700

    add Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,25 @@
+import Footer from "../layout/Footer";
+import Header from "../layout/Header";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div className="bg-[url('/NetflixBG1.png')] bg-cover bg-center py-40   ">
+        <Header />
+        <div className=" text-white text-center ">
+          <div className="text-xl font-bold ">Welcome back</div>
+          <div className="px-5 text-xl font-bold">
+            Enjoy big movie,hit series and more from THB419.
+          </div>
+          <div className="py-5">Join today.Cancel anytime.</div>
+          <button className="font-light bg-red-600 py-2 px-5 rounded-md ">
+            Finish Sign Up
+          </button>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
