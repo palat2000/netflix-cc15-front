d6ac167bce84c3a0fbfa344cef975f1baa183e4e
palat2000 <palat.tungsantitham@gmail.com>
2023-11-01 12:07:17 +0700

    fix conflict

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -1,51 +1,4 @@
 export function CollectIcon() {
-<<<<<<< HEAD
-    return (
-      <div>
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          data-name="Checkmark"
-          aria-hidden="true"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M21.2928 4.29285L22.7071 5.70706L8.70706 19.7071C8.51952 19.8946 8.26517 20 7.99995 20C7.73474 20 7.48038 19.8946 7.29285 19.7071L0.292847 12.7071L1.70706 11.2928L7.99995 17.5857L21.2928 4.29285Z"
-            fill="currentColor"
-          ></path>
-        </svg>
-      </div>
-    );
-  }
-  export function CheckIcon() {
-    return (
-      <div className="w-8">
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-          <g
-            id="SVGRepo_tracerCarrier"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></g>
-          <g id="SVGRepo_iconCarrier">
-            {" "}
-            <rect width="24" height="24" fill="white"></rect>{" "}
-            <path
-              d="M5 13.3636L8.03559 16.3204C8.42388 16.6986 9.04279 16.6986 9.43108 16.3204L19 7"
-              stroke="#E60914"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></path>{" "}
-          </g>
-        </svg>{" "}
-      </div>
-    );
-  }
-=======
   return (
     <div>
       <svg
@@ -78,17 +31,15 @@ export function CheckIcon() {
           strokeLinejoin="round"
         ></g>
         <g id="SVGRepo_iconCarrier">
-          {" "}
-          <rect width="24" height="24" fill="white"></rect>{" "}
+          <rect width="24" height="24" fill="white"></rect>
           <path
             d="M5 13.3636L8.03559 16.3204C8.42388 16.6986 9.04279 16.6986 9.43108 16.3204L19 7"
             stroke="#E60914"
             strokeLinecap="round"
             strokeLinejoin="round"
-          ></path>{" "}
+          ></path>
         </g>
-      </svg>{" "}
+      </svg>
     </div>
   );
 }
->>>>>>> develop
