696d8cc35126ad2723a0e7739e0966762c5df07b
Koblamer <RapeepornT.mar@gmail.com>
2023-11-01 14:42:06 +0700

    add vdo for userbrowse

--- src/components/Browse/MainTrailer.jsx
+++ src/components/Browse/MainTrailer.jsx
@@ -1,18 +1,12 @@
 export default function MainTrailer() {
   return (
     <div>
-      <img
-        src="https://occ-0-1214-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABfrrPL_GMLbBgSfXJYIU7TUzY7W0EnkJP106-lljvrQWMJmUI9kTB-Ybb_Wlu-zVjcb4CnQGqCn1Ixbvx4VqtY_dK_fPRwpWYGK9.webp?r=c5b"
-        width={1280}
-      ></img>
-
-      {/* <video
-        src="https://www.netflix.com/7dae1c88-6af1-4945-a1d0-6fdf8fea7cd1"
-        style=""
-        width={1280}
-        height={720}
-        controls
-      /> */}
+      <video width="1280" height="720" controls>
+        <source
+          src="https://res.cloudinary.com/diyiw4pvv/video/upload/v1698596570/romantic/yehfohhwfpbe8m2r4hwg.mp4"
+          type="video/mp4"
+        />
+      </video>
     </div>
   );
 }
