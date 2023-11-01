5ad037039c94213f2dcb1194fec5b7c4ffdfca55
Krid-Chaichaloemdet <kridtue01@hotmail.com>
2023-11-01 14:08:30 +0700

    fixhomepage

--- src/pages/ManageProfilesPage.jsx
+++ src/pages/ManageProfilesPage.jsx
@@ -1,13 +1,19 @@
 import { TiPencil } from "react-icons/ti";
 import { FaPlusCircle } from "react-icons/fa";
+import ManageProfileModal from "../pages/ManageProfileModal";
+import { useState } from "react";
 
 export default function ManageProfiles() {
+  const [IsOpenModal, setIsOpenModal] = useState(false);
   return (
-    <div className="flex flex-col bg-black items-center h-full p-10 gap-5 ">
+    <div className="flex flex-col bg-black items-center h-full p-10 gap-5 relative">
       <div className="text-white text-2xl">Manage Profiles:</div>
       <div className="flex flex-row flex-wrap gap-2">
-        <div className="cursor-pointer  flex flex-col items-center p-1  group">
-          <TiPencil className="absoluteb translate-y-12 z-20 text-white text-lg  " />
+        <div
+          onClick={() => setIsOpenModal(!IsOpenModal)}
+          className="cursor-pointer  flex flex-col items-center p-1  group"
+        >
+          <TiPencil className=" translate-y-12 z-20 text-white text-lg  " />
           <img
             className="h-20 rounded-sm relative opacity-70 group-hover:border"
             src="https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png"
@@ -19,7 +25,7 @@ export default function ManageProfiles() {
           </div>
         </div>
         <div className="cursor-pointer  flex flex-col items-center p-1 group">
-          <TiPencil className="absoluteb translate-y-12 z-20 text-white text-lg" />
+          <TiPencil className=" translate-y-12 z-20 text-white text-lg" />
           <img
             className="h-20 rounded-sm relative opacity-70 group-hover:border"
             src="https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png"
@@ -31,7 +37,7 @@ export default function ManageProfiles() {
           </div>
         </div>
         <div className="cursor-pointer  flex flex-col items-center p-1 group">
-          <TiPencil className="absoluteb translate-y-12 z-20 text-white text-lg" />
+          <TiPencil className=" translate-y-12 z-20 text-white text-lg" />
           <img
             className="h-20 rounded-sm relative opacity-70 group-hover:border"
             src="https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png"
@@ -43,7 +49,7 @@ export default function ManageProfiles() {
           </div>
         </div>
         <div className="cursor-pointer  flex flex-col items-center p-1 group">
-          <TiPencil className="absoluteb translate-y-12 z-20 text-white text-lg" />
+          <TiPencil className=" translate-y-12 z-20 text-white text-lg" />
           <img
             className="h-20 rounded-sm relative opacity-70 group-hover:border"
             src="https://i.pinimg.com/originals/b0/90/cb/b090cba82229ba7a9f4df22ec8f3ea34.png"
@@ -54,7 +60,10 @@ export default function ManageProfiles() {
             Children
           </div>
         </div>
-        <div className="cursor-pointer  flex flex-col items-center p-1 group gap-1">
+        <div
+          onClick={() => setIsOpenModal(!IsOpenModal)}
+          className="cursor-pointer  flex flex-col items-center p-1 group gap-1"
+        >
           <FaPlusCircle className="absolute translate-y-10 z-20 text-gray-500 group-hover:text-gray-500 text-4xl" />
           <div className=" h-20 w-20 translate-y-5  rounded-sm  group-hover:border opacity-0 group-hover:opacity-75 bg-white border-white"></div>
 
@@ -66,6 +75,7 @@ export default function ManageProfiles() {
       <div className="pl-4 pr-4 text-xs p-1 bg-white hover:bg-red-700 hover:text-white  hover:cursor-pointer">
         Done
       </div>
+      {IsOpenModal && <ManageProfileModal onClose={setIsOpenModal} />}
     </div>
   );
 }
