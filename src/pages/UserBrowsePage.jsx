c0d231a5caa5a5ed37087cd1e968855b8d4dc4ba
palat2000 <palat.tungsantitham@gmail.com>
2023-11-02 15:50:52 +0700

    fix update sub

--- src/pages/UserBrowsePage.jsx
+++ src/pages/UserBrowsePage.jsx
@@ -1,16 +1,31 @@
 import { useEffect } from "react";
 import Swal from "sweetalert2";
+import axios from "../config/axios";
 
 function UserBrowsePage() {
   useEffect(() => {
     const query = new URLSearchParams(window.location.search);
+    const sessionId = query.get("session_id");
+    const subscription = async (sessionId) => {
+      try {
+        await axios.post(`/payment/success-subscription/${sessionId}`);
+        Swal.fire({
+          title: "Payment successful",
+          text: "Subscription success",
+          icon: "success",
+          confirmButtonText: "close",
+        });
+      } catch (err) {
+        Swal.fire({
+          title: "Error",
+          text: err.message,
+          icon: "error",
+          confirmButtonText: "Close",
+        });
+      }
+    };
     if (query.get("success") === "true") {
-      Swal.fire({
-        title: "Payment successful",
-        text: "Subscription success",
-        icon: "success",
-        confirmButtonText: "close",
-      });
+      subscription(sessionId);
     }
   }, []);
   return <div>UserBrowsePage</div>;
