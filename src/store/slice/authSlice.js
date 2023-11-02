06dc873fea06b106dd1612d54700bb8af218bd1f
palat2000 <palat.tungsantitham@gmail.com>
2023-11-02 20:36:36 +0700

    getMeAction

--- src/store/slice/authSlice.js
+++ src/store/slice/authSlice.js
@@ -1,69 +1,91 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "../../config/axios";
-import { registerUser,loginUser } from "../utils/userApi";
+import { registerUser, loginUser, getMe } from "../utils/userApi";
 import { useNavigate } from "react-router-dom";
 import { addAccessToken } from "../../utils/local-storage";
 
 const initialState = {
   error: null,
   loading: false,
-  data: {}
+  data: {},
 };
 
+export const registerAction = createAsyncThunk(
+  "/auth/register",
+  async (input) => {
+    try {
+      let res = await registerUser(input);
+      return res;
+    } catch (error) {
+      throw error.response.data;
+    }
+  }
+);
 
-export const registerAction = createAsyncThunk('/auth/register', async (input) => {
+export const loginAction = createAsyncThunk("auth/login", async (input) => {
   try {
-    let res = await registerUser(input)
-    return res
+    let res = await loginUser(input);
+    return res;
   } catch (error) {
-    throw error.response.data
+    throw error.response.data;
   }
-})
+});
 
-export const loginAction = createAsyncThunk('auth/login',async (input) =>{
-  try{
-    let res = await loginUser(input)
-    return res
-  }catch(error){
-    throw error.response.data
-  }
-})
+export const getMeAction = createAsyncThunk("auth/me", async () => {
+  const res = await getMe();
+  return res;
+});
 
 export const authSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {resetState: (state) => {
-    state.error = null;}
+  reducers: {
+    resetState: (state) => {
+      state.error = null;
+    },
   },
-  extraReducers: builder => {
+  extraReducers: (builder) => {
     builder
       .addCase(registerAction.fulfilled, (state, action) => {
-        state.loading = false
-        state.data = action.payload
+        state.loading = false;
+        state.data = action.payload.user;
       })
       .addCase(registerAction.pending, (state, action) => {
-        state.loading = null;
+        state.error = null;
         state.loading = true;
-        
       })
       .addCase(registerAction.rejected, (state, action) => {
-        state.error = action.error
-        state.loading = false
+        state.error = action.error;
+        state.loading = false;
       })
       .addCase(loginAction.fulfilled, (state, action) => {
-        state.loading = false
-        state.data = action.payload
+        state.loading = false;
+        state.data = action.payload.user;
       })
       .addCase(loginAction.pending, (state, action) => {
-        state.loading = null;
-        state.loading = true
+        state.error = null;
+        state.loading = true;
       })
       .addCase(loginAction.rejected, (state, action) => {
-        state.error = action.error
-        state.loading = false
+        state.error = action.error;
+        state.loading = false;
       })
-
-  }
+      .addCase(getMeAction.pending, (state, action) => {
+        state.error = null;
+        state.loading = true;
+        state.data = {};
+      })
+      .addCase(getMeAction.fulfilled, (state, action) => {
+        state.error = null;
+        state.loading = false;
+        state.data = action.payload.user;
+      })
+      .addCase(getMeAction.rejected, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.data = {};
+      });
+  },
 });
 export const { resetState } = authSlice.actions;
-export default authSlice.reducer
+export default authSlice.reducer;
