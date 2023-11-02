38444dd5358fdc764bb41c2056fb5c35c0513092
Koblamer <RapeepornT.mar@gmail.com>
2023-11-02 14:57:34 +0700

    addMoreInfo

--- src/components/Browse/MoreInfoButton.jsx
+++ src/components/Browse/MoreInfoButton.jsx
@@ -4,7 +4,7 @@ import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 export default function MoreInfoButton() {
   return (
     <div>
-      <button className=" text-white text-xs bg-slate-300  my-1 py-1.5 px-2 rounded-md hover:bg-transparent">
+      <button className=" text-white text-xs0  my-1 py-1.5 px-2 rounded-sm  bg-white bg-opacity-30 hover:bg-opacity-20 ">
         <FontAwesomeIcon icon={faInfoCircle} className="mr-2" />
         More Info
       </button>
