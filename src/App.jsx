af178f9a0c40e794b3f6c7f6452bdfbf2b6304ad
palat2000 <palat.tungsantitham@gmail.com>
2023-11-03 11:08:54 +0700

    update 03/11

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useEffect } from "react";
 import Route from "./router/Route";
 import { useDispatch, useSelector } from "react-redux";
 import { getMeAction } from "./store/slice/authSlice";
+import { getAccessToken } from "./utils/local-storage";
 
 function App() {
   const dispatch = useDispatch();
   const { loading } = useSelector((store) => store.user);
 
   useEffect(() => {
-    dispatch(getMeAction());
+    if (getAccessToken()) {
+      dispatch(getMeAction());
+    }
   }, []);
 
   if (loading) return <h1>Loading...</h1>;
--- src/features/logo&button/NetflixLogo.jsx
+++ src/features/logo&button/NetflixLogo.jsx
@@ -1,12 +1,15 @@
-export default function NetflixLogo() {
+import { useNavigate } from "react-router-dom";
 
-    
-    return (
-        <img
-            onClick={() => {
-                return navigate("/");
-            }}
-            className="w-44 cursor-pointer pl-7"
-            src="https://pngimg.com/uploads/netflix/netflix_PNG25.png"
-        ></img>)
+export default function NetflixLogo() {
+  const navigate = useNavigate();
+
+  return (
+    <img
+      onClick={() => {
+        return navigate("/");
+      }}
+      className="w-44 cursor-pointer pl-7"
+      src="https://pngimg.com/uploads/netflix/netflix_PNG25.png"
+    ></img>
+  );
 }
