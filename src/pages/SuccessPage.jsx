be3ea10403a1ea0b09d04dec0ec6a0d30b826ca2
palat2000 <palat.tungsantitham@gmail.com>
2023-11-04 16:15:31 +0700

    add successPage

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -4,7 +4,11 @@ function SuccessPage() {
   return (
     <div className="h-full">
       <HeaderAuthPage />
-      <div className="flex flex-col justify-center items-center"></div>
+      <div className="flex flex-col justify-center items-center">
+        <h2>Payment successful</h2>
+        <h3>Subscription success</h3>
+        <button>Continue</button>
+      </div>
     </div>
   );
 }
