8e05cda8deaf4e929b135641c54ae5d317b6a66b
jongbeeq <titivutewhangsukij@gmail.com>
2023-11-07 09:56:27 +0700

    updateToUseDispatch

--- src/feature/TrailerLayoutRight.jsx
+++ src/feature/TrailerLayoutRight.jsx
@@ -1,31 +1,29 @@
-import { useState } from "react";
-import CloseButton from "../components/button/CloseButton";
-import MuteButton from "../components/button/new/MuteButton";
-import SoundOnButton from "../components/button/SoundOnButton";
+import CloseButton from '../components/button/CloseButton';
+import MuteButton from '../components/button/MuteButton';
+import SoundOnButton from '../components/button/SoundOnButton';
+import { useSelector, useDispatch } from 'react-redux'
+import { closeModal, toggleMute } from "../store/slice/contentSlice"
 
-export default function TrailerLayoutRight() {
-  const [isMute, setIsMute] = useState(true);
-  const { trailerPlayer, handleClickClose } = useContentModal();
-  const clickSwapMute = () => {
-    setIsMute(!isMute);
-    trailerPlayer.current.muted = !isMute;
-  };
+export default function TrailerLayoutRight({ movieId }) {
+  const trailerIsMute = useSelector(state => state.content[movieId]?.trailerIsMute)
+  const dispatch = useDispatch()
+
 
   return (
-    <div className="flex flex-col justify-between items-center h-full">
-      <div onClick={handleClickClose}>
+    <div className='flex flex-col justify-between items-center h-full'>
+      <div onClick={() => dispatch(closeModal())}>
         <CloseButton />
       </div>
-      {isMute && (
-        <div onClick={clickSwapMute}>
+      {trailerIsMute && (
+        <div onClick={() => dispatch(toggleMute(movieId))}>
           <MuteButton />
         </div>
       )}
-      {!isMute && (
-        <div onClick={clickSwapMute}>
+      {!trailerIsMute && (
+        <div onClick={() => dispatch(toggleMute(movieId))}>
           <SoundOnButton />
         </div>
       )}
     </div>
-  );
+  )
 }
