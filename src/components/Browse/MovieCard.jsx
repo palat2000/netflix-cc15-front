e27001cf26c953ec659f1747056758f4e3d6b407
Koblamer <RapeepornT.mar@gmail.com>
2023-11-09 01:08:14 +0700

    Add hover movie card

--- src/components/Browse/MovieCard.jsx
+++ src/components/Browse/MovieCard.jsx
@@ -1,14 +1,9 @@
-import AddToListButton from "../Button/AddToListButton";
-import LikeButton from "../Button/LikeButton";
-import MoreInfoCircleButton from "../Button/MoreInfoCircleButton";
-import PlayCircleButton from "../Button/PlayCircleButton";
-
 export default function MovieCard({ movie }) {
   return (
-    <div className="relative flex flex-col rounded-sm  bg-zinc-900 w-fit ">
+    <div className="relative flex flex-col rounded-sm  bg-zinc-900  ">
       <video
-        className="rounded-t-md "
-        width="250"
+        className="rounded-t-md"
+        width="200"
         height="auto"
         muted
         loop
@@ -17,16 +12,6 @@ export default function MovieCard({ movie }) {
       >
         <source src={movie?.trailer} type="video/mp4" />
       </video>
-      <div className="flex justify-between m-2 ">
-        <div className="flex items-center">
-          <PlayCircleButton customizeClass={" scale-90"} />
-          <AddToListButton customizeClass={"scale-90"} />
-          <LikeButton customizeClass={"scale-90"} />
-        </div>
-        <MoreInfoCircleButton customizeClass={"scale-90"} />
-      </div>
-      <div className="text-white text-xs">Movie Name</div>
-      <div className="text-white text-xs">Genres</div>
     </div>
   );
 }
