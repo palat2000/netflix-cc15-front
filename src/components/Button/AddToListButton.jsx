97acbac11648215272196bf8d187608e89625030
Koblamer <RapeepornT.mar@gmail.com>
2023-11-08 14:22:09 +0700

    add MovieCard

--- src/components/Button/AddToListButton.jsx
+++ src/components/Button/AddToListButton.jsx
@@ -1,14 +1,13 @@
-import IconButton from "@mui/material/IconButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function AddToListButton({ customizeClass }) {
   return (
-    <IconButton className={`${customizeClass} w-fit h-fit `}>
+    <div className={`${customizeClass}`}>
       <FontAwesomeIcon
         icon={faPlus}
-        className="text-neutral-300 bg-neutral-700 bg-opacity-80 border border-neutral-400 rounded-full  p-1  hover:text-neutral-300 hover:border-neutral-100 hover:bg-neutral-600 duration-100"
+        className="text-neutral-300 bg-zinc-900 bg-opacity-80 border border-neutral-400 rounded-full  hover:text-neutral-300 hover:border-neutral-300 hover:bg-neutral-60  p-2 scale-100 duration-100"
       />
-    </IconButton>
+    </div>
   );
 }
