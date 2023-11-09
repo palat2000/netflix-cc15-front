b6325fafdbe52b9652a401caffb15e3564b54714
palat2000 <palat.tungsantitham@gmail.com>
2023-11-09 14:03:42 +0700

    success page

--- src/pages/UserBrowsePage.jsx
+++ src/pages/UserBrowsePage.jsx
@@ -3,8 +3,6 @@ import ContentModalContextProvider from "../feature/context/ContentModalContext"
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchAllContent } from "../store/slice/allContentSlice";
-import Swal from "sweetalert2";
-import axios from "../config/axios";
 
 import MainTrailer from "../components/Browse/MainTrailer";
 import NavbarAdult from "../components/Browse/NavbarAdult";
@@ -20,29 +18,6 @@ function UserBrowsePage() {
 
   const { error, loading, data } = useSelector((store) => store.allContent);
   useEffect(() => {
-    const query = new URLSearchParams(window.location.search);
-    const sessionId = query.get("session_id");
-    const subscription = async (sessionId) => {
-      try {
-        await axios.post(`/payment/success-subscription/${sessionId}`);
-        Swal.fire({
-          title: "Payment successful",
-          text: "Subscription success",
-          icon: "success",
-          confirmButtonText: "close",
-        });
-      } catch (err) {
-        Swal.fire({
-          title: "Error",
-          text: err.message,
-          icon: "error",
-          confirmButtonText: "Close",
-        });
-      }
-    };
-    if (query.get("success") === "true") {
-      subscription(sessionId);
-    }
     dispatch(fetchAllContent());
   }, []);
 
