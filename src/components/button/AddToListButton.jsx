dbf2d35f0ee3b595f71ba26d2636cc63a02c1c5e
palat2000 <palat.tungsantitham@gmail.com>
2023-11-10 17:36:31 +0700

    haha

--- src/components/button/AddToListButton.jsx
+++ src/components/button/AddToListButton.jsx
@@ -8,6 +8,7 @@ export default function AddToListButton({ customizeClass }) {
         icon={faPlus}
         className="text-neutral-300 bg-zinc-900 bg-opacity-80 border border-neutral-400 rounded-full  hover:text-neutral-300 hover:border-neutral-300 hover:bg-neutral-60 p-2 scale-100 duration-100"
       />
+      <div></div>
     </div>
   );
 }
