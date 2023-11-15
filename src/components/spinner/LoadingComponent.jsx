55fe500d84270a6b60ea4c3c5eaa4d530da65fe1
palat2000 <palat.tungsantitham@gmail.com>
2023-11-15 17:03:07 +0700

    fix modal

---
+++ src/components/spinner/LoadingComponent.jsx
@@ -0,0 +1,9 @@
+function LoadingComponent() {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="w-10 h-10 border-2 border-t-primary border-l-[#e5091468] border-r-[#e5091468] border-b-[#e5091468] rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default LoadingComponent;
