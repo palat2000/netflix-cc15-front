284e28447636c28e6b6979e1c83b92d110a71820
jongbeeq <titivutewhangsukij@gmail.com>
2023-11-16 08:19:38 +0700

    hoverEpisodeThenShowPlayIcon

--- src/feature/EpisodeBox.jsx
+++ src/feature/EpisodeBox.jsx
@@ -1,14 +1,22 @@
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import HoverPlayButton from "../components/button/HoverPlayButton";
 
 export default function EpisodeBox({ movie }) {
     const videoEpisode = useRef(null);
     const [videoDuration, setVideoDuration] = useState(null);
     const recentWatchingEpisode = useSelector(store => store?.content?.data?.movie?.recentWatchingEpisode?.videoId)
-    console.log("🚀 ~ file: EpisodeBox.jsx:9 ~ EpisodeBox ~ recentWatchingEpisode:", recentWatchingEpisode)
-    console.log('first', movie)
     const [isRecentWatching, setIsRecenetWatching] = useState(false)
+
+    const [play, setPlay] = useState(false)
+
+    const handleOnMouseEnter = () => {
+        setPlay(true)
+    }
+    const handleOnMouseOut = () => {
+        setPlay(false)
+    }
 
 
     useEffect(() => {
@@ -25,13 +33,18 @@ export default function EpisodeBox({ movie }) {
 
     return (
         <Link to={`/watch/${movie?.id}`}>
-            <div className={`border-b-[1px] border-neutral-700 rounded-[4px] ${isRecentWatching && "bg-neutral-800"}`}>
+            <div className={`border-b-[1px] border-neutral-700 rounded-[4px] ${isRecentWatching && "bg-neutral-800"}`} onMouseEnter={handleOnMouseEnter} onMouseLeave={handleOnMouseOut}>
                 <div className="w-full flex items-center p-4 rounded-md">
                     <div className="text-neutral-400 text-2xl">{movie?.videoEpisodeNo}</div>
-                    <div className="h-full w-[20%]">
+                    <div className="h-full w-[20%] relative">
                         <video ref={videoEpisode}>
                             <source src={movie?.videoUrl}></source>
                         </video>
+                        {play &&
+                            <div className="w-full h-full z-10 absolute flex justify-center items-center top-0">
+                                <HoverPlayButton />
+                            </div>
+                        }
                     </div>
                     <div className="flex flex-col justify-between w-[70%] gap-2 pl-4">
                         <div className="flex justify-between">
