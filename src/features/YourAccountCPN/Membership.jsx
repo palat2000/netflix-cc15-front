20a76be3373d739d8e81d62b1927b6974930d612
palat2000 <palat.tungsantitham@gmail.com>
2023-11-16 15:32:42 +0700

    finish

--- src/features/YourAccountCPN/Membership.jsx
+++ src/features/YourAccountCPN/Membership.jsx
@@ -1,11 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 import {
   cancelSubscription,
   createSubscription,
-  resumeSubscription,
 } from "../../store/slice/authSlice";
+import ConfirmModal from "./ConfirmModal";
 
 export default function Membership() {
+  const [confirmCancel, setConfirmCancel] = useState(false);
+  const [confirmRestart, setConfirmRestart] = useState(false);
   const dispatch = useDispatch();
   const user = useSelector((state) => {
     return state?.user?.data.user;
@@ -13,26 +16,42 @@ export default function Membership() {
 
   const handleCancelSubscription = () => {
     dispatch(cancelSubscription());
+    setConfirmCancel(false);
   };
 
   const handleRestartSubscription = () => {
     dispatch(createSubscription());
+    setConfirmRestart(false);
   };
 
   return (
     <div className="flex justify-between pt-10 ">
+      {confirmCancel && (
+        <ConfirmModal
+          handleClose={() => setConfirmCancel(false)}
+          handleFunction={handleCancelSubscription}
+          message="Do you need to cancel subscription?"
+        />
+      )}
+      {confirmRestart && (
+        <ConfirmModal
+          handleClose={() => setConfirmRestart(false)}
+          handleFunction={handleRestartSubscription}
+          message="Do you need to restart subscription?"
+        />
+      )}
       <div className="pr-10 w-2/6">
         <div className="text-[#737373]">MEMBERSHIP & BILLING</div>
         {user.isActive ? (
           <button
-            onClick={() => handleCancelSubscription()}
+            onClick={() => setConfirmCancel(true)}
             className="bg-[#E5E5E5] hover:bg-slate-100 pl-10 pr-10 pt-2 pb-2 rounded-sm drop-shadow-md text-sm"
           >
             Cancel Membership
           </button>
         ) : (
           <button
-            onClick={() => handleRestartSubscription()}
+            onClick={() => setConfirmRestart(true)}
             className="bg-[#E5E5E5] hover:bg-slate-100 pl-10 pr-10 pt-2 pb-2 rounded-sm drop-shadow-md text-sm"
           >
             Restart Membership
