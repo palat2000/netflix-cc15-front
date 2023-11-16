592639e55e3201fdf746d8a5364072a80a8e150d
palat2000 <palat.tungsantitham@gmail.com>
2023-11-16 22:14:25 +0700

    finish notification

--- src/components/Browse/MainTrailer.jsx
+++ src/components/Browse/MainTrailer.jsx
@@ -3,10 +3,10 @@ import MovieDetails from "./MovieDetails";
 export default function MainTrailer({ mainTrailerMovie }) {
   return (
     <div>
-      <div className="relative  ">
+      <div className="relative">
         <MovieDetails movie={mainTrailerMovie} />
         <video
-          width="2000"
+          width="100%"
           className=""
           height="auto"
           loop
