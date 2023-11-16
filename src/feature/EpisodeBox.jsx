7609de260d44626c204762503d9a5a163f47f162
jongbeeq <titivutewhangsukij@gmail.com>
2023-11-16 07:55:08 +0700

    canClickEpisodeInModal

--- src/feature/EpisodeBox.jsx
+++ src/feature/EpisodeBox.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function EpisodeBox({ movie }) {
     const videoEpisode = useRef(null);
@@ -14,24 +15,26 @@ export default function EpisodeBox({ movie }) {
     }, []);
 
     return (
-        <div className="border-b-[1px] border-neutral-600">
-            <div className="w-full flex items-center p-4 rounded-md">
-                <div className="text-neutral-400 text-2xl">{movie?.videoEpisodeNo}</div>
-                <div className="h-full w-[20%]">
-                    <video ref={videoEpisode}>
-                        <source src={movie?.videoUrl}></source>
-                    </video>
-                </div>
-                <div className="flex flex-col justify-between w-[70%] gap-2 pl-4">
-                    <div className="flex justify-between">
-                        <div className="text-sm font-medium">{movie?.videoEpisodeName}</div>
-                        <div className="text-sm font-medium">{videoDuration !== null ? videoDuration + 'm' : null}</div>
+        <Link to={`/watch/${movie?.id}`}>
+            <div className="border-b-[1px] border-neutral-600">
+                <div className="w-full flex items-center p-4 rounded-md">
+                    <div className="text-neutral-400 text-2xl">{movie?.videoEpisodeNo}</div>
+                    <div className="h-full w-[20%]">
+                        <video ref={videoEpisode}>
+                            <source src={movie?.videoUrl}></source>
+                        </video>
                     </div>
-                    <div className="text-xs text-neutral-400">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores amet commodi aspernatur doloremque at itaque quis eos earum sunt error autem voluptate, quo sequi ipsam ullam et rem nobis repudiandae?
+                    <div className="flex flex-col justify-between w-[70%] gap-2 pl-4">
+                        <div className="flex justify-between">
+                            <div className="text-sm font-medium">{movie?.videoEpisodeName}</div>
+                            <div className="text-sm font-medium">{videoDuration !== null ? videoDuration + 'm' : null}</div>
+                        </div>
+                        <div className="text-xs text-neutral-400">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores amet commodi aspernatur doloremque at itaque quis eos earum sunt error autem voluptate, quo sequi ipsam ullam et rem nobis repudiandae?
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
