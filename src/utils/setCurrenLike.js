3fc4ed735b5978defab9cc30cc6a1af8ef3c11b2
jongbeeq <titivutewhangsukij@gmail.com>
2023-11-17 11:01:35 +0700

    setStartLike

--- src/utils/setCurrenLike.js
+++ src/utils/setCurrenLike.js
@@ -1,13 +1,18 @@
 export default function setCurrenLike(movieId, allMovieData, newLikeData) {
+    console.log("🚀 ~ file: setCurrenLike.js:2 ~ setCurrenLike ~ newLikeData:", newLikeData)
+    console.log("🚀 ~ file: setCurrenLike.js:2 ~ setCurrenLike ~ allMovieData:", allMovieData)
+    console.log("🚀 ~ file: setCurrenLike.js:2 ~ setCurrenLike ~ movieId:", movieId)
     let newAllMovieData
     for (const key in allMovieData) {
+        console.log(allMovieData[key])
         newAllMovieData = allMovieData[key].map(el => {
+            console.log(el)
             if (el.id === movieId) {
                 el.likeMovie = newLikeData
             }
             return el
         })
     }
-
+    console.log("🚀 ~ file: setCurrenLike.js:3 ~ setCurrenLike ~ newAllMovieData:", newAllMovieData)
     return newAllMovieData
 }
